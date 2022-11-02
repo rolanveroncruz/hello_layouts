ff51e474c60b972a0ea4e879961da0396b1b8914
Rolan Veron Cruz <rolan@upshot.global>
2022-11-02 14:20:06 +0800

    finished basic_layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,18 +9,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          // ignore: prefer_const_constructors
-          title: Text("Building Layouts with Flutter"),
-        ),
-        body: const Center(
-          // ignore: sort_child_properties_last
-          child: Text("Hello Flutter Layouts",
-          style: TextStyle(fontSize:24),),
-        )
-      )
-    );
+    return MaterialApp(
+        theme: ThemeData.light(), 
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Building Layouts with Flutter"),
+            ),
+            body: const Center(
+              // ignore: sort_child_properties_last
+              child: Text(
+                "Hello Flutter Layouts",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.lightbulb_outline),
+              onPressed: () {
+                // ignore: avoid_print
+                print('You rang?');
+              },),
+              persistentFooterButtons: [
+                IconButton(icon: Icon(Icons.add_comment),onPressed: () {}),
+                IconButton(icon: Icon(Icons.add_alarm),onPressed: () {}),
+                IconButton(icon: Icon(Icons.add_location),onPressed: () {}),
+
+              ],));
   }
 }
